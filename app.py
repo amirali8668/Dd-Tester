9af735d24eb9b1e7dcdf2a3ee8885a0b7383f226
Amir <145401865+amirali8668@users.noreply.github.com>
2026-09-18 16:03:22 +0330

    Update app.py

--- app.py
+++ app.py
@@ -202,5 +202,17 @@ def handle_stop():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    socketio.run(app, host="0.0.0.0", port=port)
+
+    port = int(
+        os.environ.get(
+            "PORT",
+            "5000"
+        )
+    )
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        allow_unsafe_werkzeug=True
+    )
